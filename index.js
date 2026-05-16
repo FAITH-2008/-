edcee94c824af87c6ab2e76938890c53070fc518
FAITH-2008 <elegantfaith43@gmail.com>
2026-05-16 12:51:51 +0100

    Enable QR code printing in terminal

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function startBot() {
 
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: false,
+        printQRInTerminal: true,
         logger: P({ level: "silent" })
     });
 
